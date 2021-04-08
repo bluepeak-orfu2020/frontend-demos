7e5a43eda50251763f42ab83d0ef35e35bed2790
Erik Stenbäcka <Erik.Stenbacka@sigma.se>
2021-04-08 08:27:08 +0200

    Adds api for slow requests

---
+++ SlowBackend/SlowBackend/Controllers/SlowController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace SlowBackend.Controllers
+{
+    public class SlowController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] string data)
+        {
+            Console.WriteLine($"Got data {data}");
+            Thread.Sleep(5000);
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ SlowBackend/SlowBackend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SlowBackend.WebApiApplication" Language="C#" %>
